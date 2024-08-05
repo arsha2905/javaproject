e4c56776248453862b805bd7a1998d728563fc36
arsha <arsha@192.168.1.43>
2024-08-05 20:47:55 +0530

    changes

--- MyJavaProject/src/learningJava/OddEven.java
+++ MyJavaProject/src/learningJava/OddEven.java
@@ -4,7 +4,7 @@ public class OddEven {
 	public static void main (String args[]) {
 		int x=21;
 		if(x%2==0){
-			System.out.println("The given number even");
+			System.out.println("The given number is even");
 		}
 			else{
 				System.out.println("The given number is odd");
